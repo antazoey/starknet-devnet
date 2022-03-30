79c3456ae34880dd6dd2d1aeea34937756b2404b
Antun Badurina <antun.badurina@gmail.com>
2022-03-30 12:34:46 +0200

    Skip postman tests

--- test/test_postman.py
+++ test/test_postman.py
@@ -25,6 +25,8 @@ ETH_CONTRACTS_PATH = "../starknet-hardhat-example/artifacts/contracts"
 STARKNET_MESSAGING_PATH = f"{ETH_CONTRACTS_PATH}/MockStarknetMessaging.sol/MockStarknetMessaging.json"
 L1L2_EXAMPLE_PATH = f"{ETH_CONTRACTS_PATH}/L1L2.sol/L1L2Example.json"
 
+pytest.skip("Skipping test_postman.py")
+
 @pytest.fixture(autouse=True)
 def run_before_and_after_test():
     """Run devnet before and kill it after the test run"""
